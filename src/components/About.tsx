9ff8fde502123129ece51c1e4509640f54f4f8dc
Marlon De La Roche <marlondelaroch3@gmail.com>
2023-08-25 15:08:07 -0500

    fix: :bug: remove h1 nested in an h1
    This h1 nested was generating an hydration error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const About = () => {
     return (
         <section id="about" className="h-screen flex flex-col items-center">
-            <h1 className={`${montserrat.className} text-center mb-7 font-medium text-3xl text-whiteCustom`}>Hola, soy<h1 className='font-semibold'>Marlon</h1></h1>
+            <h1 className={`${montserrat.className} text-center mb-7 font-medium text-3xl text-whiteCustom`}>Hola, soy<p className='font-semibold'>Marlon</p></h1>
             <p className={`${hind_madurai.className} text-whiteCustom text-sm text-justify mb-7`}>Front-End Developer Mid con experiencia en ReactJS Redux, VTEX IO y Typescript. Me gusta trabajar en nuevas funcionalidades y mejorar las existentes. He liderado equipos en diferentes rubros para los consecución de objetivos de la forma más eficiente. Participé en proyectos para grandes empresas de Estados Unidos, Colombia, Chile y México, teniendo la oportunidad de liderar uno de ellos.</p>
             <Link href="#experience" className='border border-solid border-black rounded-full animate-bounce border-whiteCustom'><Image src="/assets/arrow.png" alt='arrow to bottom' width="28" height="28" className="" /></Link>
         </section>
