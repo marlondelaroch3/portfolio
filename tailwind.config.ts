6dceb65d8a0deb58f6bb5381b70736426a283b5e
Marlon De La Roche <marlondelaroch3@gmail.com>
2023-09-01 22:12:43 -0500

    chore: :lipstick: Add percentage option to spacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
       colors: {
         whiteCustom: "#EDF9FE",
       },
+      backgroundColor: {
+        mainBgColor: "#324359",
+      },
       backgroundImage: {
         gradientCustom:
           "linear-gradient(90deg, rgba(30, 32, 51, 0.50) 0%, rgba(61, 64, 115, 0.50) 18.75%, rgba(90, 191, 163, 0.50) 85.45%, rgba(37, 236, 181, 0.50) 100%);",
@@ -13,15 +16,31 @@ const config: Config = {
       height: {
         "85": "355px",
       },
+      spacing: {
+        "5c": "5%",
+        "10c": "10%",
+        "1/6": "16.666667%",
+        "1/5": "20%",
+        "1/4": "25%",
+        "1/3": "33.333333%",
+        "2/5": "40%",
+        "1/2": "50%",
+        "3/5": "60%",
+        "2/3": "66.666667%",
+        "3/4": "75%",
+        "4/5": "80%",
+        "5/6": "83.333333%",
+        "1c": "100%",
+      },
       keyframes: {
-        "clickHere":{
-          '0%' : {transform: "scale(1)"},
-          '100%': {transform: "scale(0.5)"}
-        }
+        clickHere: {
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(0.5)" },
+        },
       },
       animation: {
-        "clickHere": "clickHere 1s ease-in-out infinite"
-      }
+        clickHere: "clickHere 1s ease-in-out infinite",
+      },
     },
   },
   content: [
